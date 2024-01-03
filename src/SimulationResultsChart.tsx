5e7adbe8adc7d3490def204e1e17f49bacecba1b
Steve Joiner <steve@daisyhill.net>
2024-01-02 19:54:09 -0500

    Units formatting tweak.

--- src/SimulationResultsChart.tsx
+++ src/SimulationResultsChart.tsx
@@ -153,8 +153,7 @@ export const Chart: Component<ChartProps> = allProps => {
     const getSeriesValue = (series: ChartData): string => {
         const value = series.data[series.data.length - 1]?.toPrecision(2) ?? '';
         if (value === undefined) return '';
-        const units = series.units ? ` ${series.units}` : '';
-        return value + units;
+        return value + series.units ?? '';
     };
 
     return (
